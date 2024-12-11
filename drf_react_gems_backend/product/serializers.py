092806b06e8942c4f4d7c6b860e5cfb18a6a8000
Beatris Ilieve <beatrisilieve@icloud.com>
2024-12-11 17:52:49 +0200

    updated product functionality

--- drf_react_gems_backend/product/serializers.py
+++ drf_react_gems_backend/product/serializers.py
@@ -1,37 +1,5 @@
 from rest_framework import serializers
-from drf_react_gems_backend.inventory.models import Inventory
 
-
-# class ProductListSerializer(serializers.ModelSerializer):
-#     model = Inventory
-#     fields = "__all__"
-
-
-# # inventory_id = serializers.IntegerField()
-# # size = serializers.CharField()
-# # price = serializers.CharField()
-# # stock_status = serializers.BooleanField()
-
-
-# class ProductDetailsSerializer(serializers.ModelSerializer):
-#     model = Inventory
-#     fields = "__all__"
-
-
-# # product_id = serializers.IntegerField()
-# # product__category_id = serializers.IntegerField()
-# # product__color_id = serializers.IntegerField()
-# # product__first_image_url = serializers.URLField()
-# # product__second_image_url = serializers.URLField()
-# # product__description = serializers.CharField()
-# # full_category_title = serializers.CharField()
-# # full_color_title = serializers.CharField()
-# # # inventory_details = InventoryDetailsSerializer(many=True)
-# # total_quantity = serializers.IntegerField()
-# # is_sold_out = serializers.BooleanField()
-
-
-from rest_framework import serializers
 
 class ProductListSerializer(serializers.Serializer):
     product_id = serializers.IntegerField()
@@ -52,6 +20,7 @@ class InventoryDetailSerializer(serializers.Serializer):
     size = serializers.CharField()
     price = serializers.CharField()
     is_sold_out = serializers.BooleanField()
+
 
 class ProductDetailsSerializer(serializers.Serializer):
     product_id = serializers.IntegerField()
