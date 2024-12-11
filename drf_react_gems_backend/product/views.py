b739c4733c214d9432c56e55f497036def98d43f
Beatris Ilieve <beatrisilieve@icloud.com>
2024-12-11 18:30:12 +0200

    updated product functionality

--- drf_react_gems_backend/product/views.py
+++ drf_react_gems_backend/product/views.py
@@ -10,44 +10,6 @@ from drf_react_gems_backend.product.serializers import (
 )
 
 
-# class ProductListApiView(api_views.ListAPIView):
-#     queryset = Inventory.objects.all()
-#     serializer_class = ProductListSerializer
-
-#     def get_queryset(self):
-
-#         queryset = super().get_queryset()
-
-#         category_pk = self.request.query_params.get("category", None)
-#         color_pk = self.request.query_params.get("color", None)
-
-#         if category_pk and color_pk:
-#             queryset = Inventory.objects.get_product_into_products_list(
-#                 category_pk=category_pk, color_pk=color_pk
-#             )
-
-#         return queryset
-
-
-# class ProductDetailsApiView(api_views.ListAPIView):
-#     queryset = Inventory.objects.all()
-#     serializer_class = ProductDetailsSerializer
-
-#     def get_queryset(self):
-
-#         queryset = super().get_queryset()
-
-#         category_pk = self.request.query_params.get("category", None)
-#         color_pk = self.request.query_params.get("color", None)
-
-#         if category_pk and color_pk:
-#             queryset = Inventory.objects.get_product_into_product_page(
-#                 category_pk=category_pk, color_pk=color_pk
-#             )
-
-#         return queryset
-
-
 class ProductStrategyType(Enum):
     PRODUCT_LIST = "product_list"
     PRODUCT_DETAILS = "product_details"
